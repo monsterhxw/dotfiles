f32d43c4692cbf46c6795cb096f02ace58912fc3
huangxuewei <huangxueweihxw@gmail.com>
2025-01-09 20:33:25 +0800

    raycast: Add AeroSpace Scripts.

---
+++ raycast/shell/aerospace-next-desktop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title AeroSpace Next Desktop
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Monster_Dump
+# @raycast.authorURL https://raycast.com/Monster_Dump
+
+/usr/local/bin/aerospace move-node-to-workspace --focus-follows-window --wrap-around next
---
+++ raycast/shell/aerospace-next-display.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title AeroSpace Next Display
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Monster_Dump
+# @raycast.authorURL https://raycast.com/Monster_Dump
+
+/usr/local/bin/aerospace move-node-to-monitor --focus-follows-window --wrap-around next
