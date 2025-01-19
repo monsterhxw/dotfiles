f9bbde74d8c539b3d5784a91dbc0e3a735547122
huangxuewei <huangxueweihxw@gmail.com>
2025-01-20 00:40:11 +0800

    aerospace: Let focus work when all windows are floating.

---
+++ aerospace/scripts/focus-window.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+AEROSPACE="/usr/local/bin/aerospace"
+
+switch_window() {
+  local direction=$1
+  local current_id=$($AEROSPACE list-windows --focused --format '%{window-id}')
+  local window_ids=($($AEROSPACE list-windows --workspace focused --format '%{window-id}'))
+  local window_count=${#window_ids[@]}
+
+  local current_index
+  for i in "${!window_ids[@]}"; do
+    if [[ "${window_ids[$i]}" == "$current_id" ]]; then
+      current_index=$i
+      break
+    fi
+  done
+
+  local target_index
+  case $direction in
+    "left"|"up")
+      target_index=$(( (current_index - 1 + window_count) % window_count ))
+      ;;
+    "right"|"down")
+      target_index=$(( (current_index + 1) % window_count ))
+      ;;
+    *)
+      exit 1
+      ;;
+  esac
+
+  $AEROSPACE focus --window-id "${window_ids[$target_index]}"
+}
+
+if [[ $# -eq 1 ]]; then
+  $AEROSPACE focus --boundaries workspace --boundaries-action wrap-around-the-workspace "$1" \
+  || switch_window "$1"
+else
+  exit 1
+fi
