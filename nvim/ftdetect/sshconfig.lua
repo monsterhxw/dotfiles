2536759314c7fbd07ed65a71cc088923ab9dadfa
huangxuewei <huangxueweihxw@gmail.com>
2025-03-10 18:05:45 +0800

    nvim: Add version 1 configurations.

--- nvim/ftdetect/sshconfig.lua
+++ nvim/ftdetect/sshconfig.lua
@@ -1,8 +1,5 @@
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = {
-    vim.env.HOME .. "/.ssh/config.d/*",
-    "*/ssh/config{,.d/*}"
-  },
+  pattern = "*/{.,}ssh/config{,.d/*}",
   callback = function()
     vim.bo.filetype = "sshconfig"
   end,
