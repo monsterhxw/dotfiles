6ab26471944fce03a3d559c891d217c0d5c8ca81
huangxuewei <huangxueweihxw@gmail.com>
2025-03-10 13:31:53 +0800

    nvim: Add version 1 configurations.

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,7 @@
+--------------------------------------------------------------------------------
+-- Lua Filetype Settings (after)
+--------------------------------------------------------------------------------
+-- Set the width of indentation to 2 spaces
+vim.bo.shiftwidth = 2
+-- Set softtabstop to -1, which makes it follow the value of shiftwidth
+vim.bo.softtabstop = -1
