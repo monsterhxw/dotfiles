1eba14e3469109981ae6b0e7266e9c230f81faa8
huangxuewei <huangxueweihxw@gmail.com>
2025-03-13 17:09:39 +0800

    nvim: Modify colorscheme color cofigurations.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,41 @@
+
+local transparent = true
+local bg = "#011628"
+local bg_dark = "#011423"
+local bg_highlight = "#143652"
+local bg_search = "#0A64AC"
+local bg_visual = "#275378"
+local fg = "#CBE0F0"
+local fg_dark = "#B4D0E9"
+local fg_gutter = "#627E97"
+local border = "#547998"
+
 return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   opts = {
-    -- style = "storm",
-    transparent = true,
-    on_highlights = function(hl, c)
-      local linenr_fg = "#627E97"
-      hl.LineNrAbove = { fg = linenr_fg }
-      hl.LineNrBelow = { fg = linenr_fg }
+    style = "night",
+    transparent = transparent,
+    on_colors = function(colors)
+      colors.bg = bg
+      colors.bg_highlight = bg_highlight
+      colors.bg_popup = bg_dark
+      colors.bg_search = bg_search
+      colors.bg_visual = bg_visual
+      colors.border = border
+
+      local bg_trsp = transparent and colors.none or bg_dark
+      colors.bg_dark = bg_trsp
+      colors.bg_float = bg_trsp
+      colors.bg_sidebar = bg_trsp
+      colors.bg_statusline = bg_trsp
+
+      colors.fg = fg
+      colors.fg_dark = fg_dark
+      colors.fg_float = fg
+      colors.fg_gutter = fg_gutter
+      colors.fg_sidebar = fg_dark
     end,
   },
   config = function(_, opts)
