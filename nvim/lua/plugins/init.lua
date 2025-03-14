a895c37a5a207132a9b5dd7ff4081c909eb77f3b
huangxuewei <huangxueweihxw@gmail.com>
2025-03-15 04:47:12 +0800

    nvim: Add vim-sleuth vim-lastplace undotree plugins.

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "tpope/vim-sleuth",
+    event = { "BufReadPre", "BufNewFile" }
+  },
+  {
+    "farmergreg/vim-lastplace",
+    event = { "BufReadPre", "BufNewFile" }
+  },
+  {
+    "mbbill/undotree",
+    cmd = { "UndotreeToggle", }
+  },
+}
