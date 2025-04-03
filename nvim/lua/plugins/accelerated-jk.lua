6145f0cdc3ad691f2cb844995f9cc61d84fef779
huangxuewei <huangxueweihxw@gmail.com>
2025-04-03 14:23:34 +0800

    nvim: Refactor accelerated-jk persistence plugin configs.

--- nvim/lua/plugins/accelerated-jk.lua
+++ nvim/lua/plugins/accelerated-jk.lua
@@ -1,9 +1,8 @@
 return {
   "rhysd/accelerated-jk",
   -- "rainbowhxch/accelerated-jk.nvim",
-  keys = { { "j", mode = "n" }, { "k", mode = "n" }, },
-  config = function(_, opts)
-    vim.keymap.set("n", "j", "<Plug>(accelerated_jk_gj)")
-    vim.keymap.set("n", "k", "<Plug>(accelerated_jk_gk)")
-  end,
+  keys = { 
+    { "j", "<Plug>(accelerated_jk_gj)", desc = "Accelerated down movement" },
+    { "k", "<Plug>(accelerated_jk_gk)", desc = "Accelerated up movement" }, 
+  },
 }
--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -1,15 +1,11 @@
 return {
   "folke/persistence.nvim",
-  enabled = false,
-  event = "BufReadPre",
+  enabled = true,
+  keys = {
+    { "<leader>qs", function() require("persistence").load() end, desc = "load the session for the current directory" },
+    { "<leader>qS", function() require("persistence").select() end, desc = "select a session to load" },
+    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "load the last session" },
+    { "<leader>qd", function() require("persistence").stop() end, desc = "stop Persistence => session won't be saved on exit" },
+  },
   opts = {},
-  config = function(_, opts)
-    local persistence = require("persistence")
-    persistence.setup(opts)
-
-    vim.keymap.set("n", "<leader>qs", function() persistence.load() end, { desc = "load the session for the current directory" })
-    vim.keymap.set("n", "<leader>qS", function() persistence.select() end, { desc = "select a session to load" })
-    vim.keymap.set("n", "<leader>ql", function() persistence.load({ last = true }) end, { desc = "load the last session" })
-    vim.keymap.set("n", "<leader>qd", function() persistence.stop() end, { desc = "stop Persistence => session won't be saved on exit" })
-  end
 }
