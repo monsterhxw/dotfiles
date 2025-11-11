b99bb7b7edda7bd7cfe2d5d94c7f142329362c5b
huangxuewei <huangxueweihxw@gmail.com>
2025-11-11 18:45:05 +0800

    nvim: Disable treesitter tmux highlight.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -25,6 +25,7 @@ return {
     ignore_install = {},
     highlight = {
       enable = true,
+      disable = { "tmux" },
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
