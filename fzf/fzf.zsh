f6f9a29ee4b6f57ae101b3322391148ac2a8b722
huangxuewei <huangxueweihxw@gmail.com>
2026-01-03 05:18:45 +0800

    chore(fzf): disable alt+c directory navigation
    - Commented out FZF_ALT_C_COMMAND and FZF_ALT_C_OPTS
    - Set FZF_ALT_C_COMMAND to empty string to disable feature
    - Disabled custom keybinding for fzf-cd-widget
    - Reorganized config sections for clarity

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -4,19 +4,6 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-# replace 'alt+c'
-bindkey "ç" fzf-cd-widget
-# for vim
-#bind -M insert "ç" fzf-cd-widget
-
-
 export FZF_DEFAULT_COMMAND='fd --type f --color=never --hidden'
 # bat doesn't work with any input other than the list of files
 # it is not a good idea to add `--preview` option
@@ -28,8 +15,9 @@ export FZF_DEFAULT_OPTS="
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always --line-range :50 {}'"
-export FZF_ALT_C_COMMAND='fd --type d . --color=never --hidden'
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
+# export FZF_ALT_C_COMMAND='fd --type d . --color=never --hidden'
+# export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
+export FZF_ALT_C_COMMAND=""
 
 export FZF_COMPLETION_TRIGGER='**'
 
@@ -37,6 +25,17 @@ export FZF_COMPLETION_TRIGGER='**'
 export FZF_TMUX=1
 export FZF_TMUX_OPTS='-p80%,60%'
 
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+# replace 'alt+c'
+# bindkey "ç" fzf-cd-widget
+# for vim
+#bind -M insert "ç" fzf-cd-widget
 
 # cdf - cd into the directory of the selected file
 cdf() {
