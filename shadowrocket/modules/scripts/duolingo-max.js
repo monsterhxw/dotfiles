e489e8c297ae29a6f60c14e3188f233f9d59ea26
huangxuewei <huangxueweihxw@gmail.com>
2026-01-17 04:25:28 +0800

    feat(shadowrocket): add duolingo max unlock module
    - Add srmodule for Duolingo Max subscription unlock
    - Add JS script to inject gold subscription data

---
+++ shadowrocket/modules/scripts/duolingo-max.js
@@ -0,0 +1,35 @@
+// Source: https://github.com/TAKAGIVEGETA/MyScripts/blob/main/lingoDuo/lingoDuo-xaM.js
+
+try {
+    let obj = JSON.parse($response.body);
+    if (!obj.responses || obj.responses.length < 2 || (obj.responses[0].headers && 'etag' in obj.responses[0].headers)); // skip
+    else {
+        const now = Math.floor(Date.now() / 1000);
+        const userdata = JSON.parse(obj.responses[0].body);
+        if (!userdata.shopItems) userdata.shopItems = [];
+        userdata.shopItems.push({
+            id: 'gold_subscription',
+            purchaseDate: now - 172800,
+            purchasePrice: 0,
+            subscriptionInfo: {
+                expectedExpiration: now + 31536000,
+                productId: "com.duolingo.DuolingoMobile.subscription.Gold.TwelveMonth.24Q2Max.168",
+                renewer: 'APPLE',
+                renewing: true,
+                tier: 'twelve_month',
+                type: 'gold'
+            }
+        });
+        userdata.subscriberLevel = 'GOLD';
+        if (!userdata.trackingProperties) userdata.trackingProperties = {};
+        userdata.trackingProperties.has_item_immersive_subscription = true;
+        userdata.trackingProperties.has_item_premium_subscription = true;
+        userdata.trackingProperties.has_item_live_subscription = true;
+        userdata.trackingProperties.has_item_gold_subscription = true;
+        userdata.trackingProperties.has_item_max_subscription = true;
+        obj.responses[0].body = JSON.stringify(userdata);
+    }
+    $done({ body: JSON.stringify(obj) });
+} catch (e) {
+    $done({});
+}
