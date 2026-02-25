b511029514d01df2419e8a78fe417a967846ec5d
huangxuewei <huangxueweihxw@gmail.com>
2026-02-25 15:12:56 +0800

    chore(nvim): update plugins and migrate nvim-surround to v4
    - Update lazy-lock.json with latest plugin versions
    - Migrate nvim-surround config to v4: remove keymaps option, use
      Plug mappings for visual mode gs/gS bindings

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -1,10 +1,14 @@
 return {
   "kylechui/nvim-surround",
   version = "*",
+  init = function()
+    -- Disable default visual mode keymaps before plugin loads
+    vim.g.nvim_surround_no_visual_mappings = true
+  end,
   event = "VeryLazy",
-  opts = {
-    keymaps = {
-      visual = "gs",
-    },
+  opts = {},
+  keys = {
+    { "gs", "<Plug>(nvim-surround-visual)", mode = "x", desc = "Surround visual selection" },
+    { "gS", "<Plug>(nvim-surround-visual-line)", mode = "x", desc = "Surround visual selection (line)" },
   },
 }
