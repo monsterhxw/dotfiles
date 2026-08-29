ad271e0de7d12be9289416a4eb1e54124bc7218a
huangxuewei <hxwpro@gmail.com>
2026-08-29 19:25:52 +0800

    fix(nvim): redraw to show flash labels immediately
    - Wrap flash search update with a forced redraw so labels paint on the
      same keypress (workaround for folke/flash.nvim#480, fix PR #488 unmerged)
    - Prevent pressing a stale on-screen label from extending the search
      instead of jumping, caused by CmdlineChanged reordering in neovim#35254
    - Update lazy-lock.json to latest plugin revisions

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -12,6 +12,24 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("flash").setup(opts)
+
+    -- Workaround for folke/flash.nvim#480 (fix PR #488 unmerged): since neovim/neovim#35254,
+    -- CmdlineChanged fires after the incsearch redraw, so new labels are not painted until the
+    -- next keypress. Pressing a label that is still on screen extends the search instead of jumping.
+    local search = require("flash.plugins.search")
+    local update = search.update
+    ---@diagnostic disable-next-line: duplicate-set-field
+    search.update = function(check_jump)
+      update(check_jump)
+      if vim.api.nvim__redraw then
+        vim.api.nvim__redraw({ flush = true })
+      else
+        vim.cmd("redraw")
+      end
+    end
+  end,
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
